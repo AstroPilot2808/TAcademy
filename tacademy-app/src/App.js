89f16b22cdf6f8b4be2d73714ea6fbeaded7231b
AstroPilot2808 <ajayohm28@gmail.com>
2023-05-23 19:41:01 -0400

    tailwind_implementation

--- tacademy-app/src/App.js
+++ tacademy-app/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-red-300">
+      <h1 className="text-2xl font-bold text-right mt-8"> to My App</h1>
+      <p style={{color: 'red'}}>This is a sample paragraph.</p>
     </div>
   );
 }
